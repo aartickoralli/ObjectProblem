e28ff427a3587c397e76f462c2147ad44787cb09
Aarti C. Koralli <aartickoralli@Aartis-MacBook-Pro.local>
2015-09-11 16:33:24 -0500

    first commit

---
+++ solo_script.js
@@ -0,0 +1,105 @@
+// ! ! !
+// Three Bugs
+
+var Atticus = { 
+               name: "Atticus",
+               employeeNumber: "2405",
+               baseSalary: "47000",
+               reviewScore: 3,
+             }
+var Jem = {
+                name:"Jem",
+                employeeNumber: "62347",
+                baseSalary: "63500", 
+                reviewScore: 4,
+              }
+var Boo = {
+                name: "Boo", 
+                employeeNumber: "11435", 
+                baseSalary: "54000",
+                reviewScore: 3,
+              }
+var Scout = {
+                  name: "Scout",
+                  employeeNumber: "6243", 
+                  baseSalary: "74750", 
+                  reviewScore: 5,
+                }
+
+var array = [Atticus, Jem, Boo, Scout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM 
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]);
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array.name;
+
+  var employeeNumber = array.employeeNumber;
+  var baseSalary = array.baseSalary;
+  var reviewScore = array.reviewScore;
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = parseInt(baseSalary * (1.0 + bonus))
+  ;
+  newArray[3] = baseSalary * bonus;
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent;
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
